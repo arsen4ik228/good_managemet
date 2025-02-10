06deb805751f7bf9d3f656465fc3eda09dfa301a
arsen4ik228 <belosheikin2003@yandex.ru>
2025-02-10 17:05:34 +0300

    fixed layout for mobile authorization

--- src/UI/app/Authorization/content/Content.jsx
+++ src/UI/app/Authorization/content/Content.jsx
@@ -170,7 +170,6 @@ export default function Content() {
   return (
     isMobile ? (
       <div className={classes.Container}>
-        <div className={classes.background}></div>
         <div className={classes.logoContainer}>
           <img src={logo} alt='logo' />
         </div>
