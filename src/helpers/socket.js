714c7a1f9cb679682ab20eb953b041e92af0b9ea
arsen4ik228 <belosheikin2003@yandex.ru>
2025-02-20 19:03:03 +0300

    create socket context

--- src/helpers/socket.js
+++ src/helpers/socket.js
@@ -1,18 +1,10 @@
 import io from "socket.io-client";
 import { baseUrl, userId } from '@helpers/constants';
 
-let socket = null;
-
 // Функция для создания соединения
 export const createConnectionSocket = () => {
     return new Promise((resolve, reject) => {
-        if (socket) {
-            resolve(socket); // Возвращаем существующий сокет, если он уже создан
-            return;
-        }
-
-        // Создаем новый сокет
-        socket = io(`${baseUrl}convert`, {
+        const socket = io(`${baseUrl}convert`, {
             cors: {
                 credentials: true,
             },
@@ -22,45 +14,26 @@ export const createConnectionSocket = () => {
             transports: ["websocket"],
         });
 
-        // Обработка события подключения
         socket.on("connect", () => {
             console.log('Socket connected');
-            resolve(socket); // Разрешаем промис, когда сокет подключится
+            resolve(socket);
         });
 
-        // Обработка события отключения
         socket.on("disconnect", (reason) => {
             console.log('Socket disconnected:', reason);
-            // Здесь можно выполнить дополнительные действия при отключении
         });
 
-        // Обработка ошибок подключения
         socket.on("connect_error", (error) => {
             console.error('Socket connection error:', error);
-            reject(error); // Отклоняем промис в случае ошибки
+            reject(error);
         });
     });
 };
 
-// Функция для подписки на событие convertCreationEvent
-export const subscribeToConvertCreationEvent = (callback) => {
-    if (socket) {
-        console.log('subscribeToConvertCreationEvent', socket)
-        socket.on("convertCreationEvent", (data) => {
-            callback(data);
-        });
-    } else {
-        console.error("Socket is not connected.");
-    }
-};
-
 // Функция для отключения сокета
-export const disconnectSocket = () => {
+export const disconnectSocket = (socket) => {
     if (socket) {
-        socket.off("connect");
-        socket.off("disconnect");
-        // socket.off("convertCreationEvent");
+        console.log('disconnect socket');
         socket.disconnect();
-        socket = null; // Сбрасываем сокет
     }
 };
