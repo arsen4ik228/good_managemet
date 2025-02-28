204e3ea7b6cd7b818d8e656fe41a678f2734fdc8
viyaya <johndoe@email.com>
2025-02-28 12:30:51 +0300

    added a post creation and post update to the companys page, created a post folder

--- src/UI/app/CompanySchema/desktop/CompanySchema.jsx
+++ src/UI/app/CompanySchema/desktop/CompanySchema.jsx
@@ -1,9 +1,19 @@
 import React, { useState, useEffect } from "react";
 import classes from "./CompanySchema.module.css";
-import Headers from "@Custom/Headers/Headers";
-import BlockSchema from "./blockSchema/BlockSchema";
-import { usePostsHook } from "@hooks/usePostsHook";
+
 import _ from "lodash";
+import { FloatButton } from "antd";
+
+import Headers from "@Custom/Headers/Headers";
+import BottomHeaders from "@Custom/Headers/BottomHeaders/BottomHeaders.jsx";
+
+import PostSchema from "./postSchema/PostSchema";
+import BlockSchema from "./blockSchema/BlockSchema";
+
+import { usePostsHook } from "@hooks/usePostsHook";
+import { useOrganizationHook } from "@hooks/useOrganizationHook";
+import arrowBack from "@image/back_white.svg";
+import DrawerCreatePost from "./drawer/DrawerCreatePost";
 
 const buildTree = (posts) => {
   const postMap = {};
@@ -29,13 +39,28 @@ const buildTree = (posts) => {
   return roots;
 };
 
-const PostTree = ({ posts }) => {
+const PostTree = ({
+  posts,
+  arrayColors,
+  setArrayColors,
+  setOnePost,
+  setViewChildrenPost,
+  setHeader,
+}) => {
   const tree = buildTree(posts);
-
+  console.log(tree);
   return (
     <div className={classes.wrapper}>
       {tree.map((post) => (
-        <BlockSchema key={post.id} post={post} />
+        <BlockSchema
+          key={post.id}
+          post={post}
+          arrayColors={arrayColors}
+          setArrayColors={setArrayColors}
+          setOnePost={setOnePost}
+          setViewChildrenPost={setViewChildrenPost}
+          setHeader={setHeader}
+        />
       ))}
     </div>
   );
@@ -48,20 +73,122 @@ export default function CompanySchema() {
     isErrorGetPosts,
   } = usePostsHook({ structure: true });
 
-  const [array, setArray] = useState([]);
+  const {
+    reduxSelectedOrganizationId,
+
+    currentOrganization,
+    isLoadingOrganizationId,
+    isFetchingOrganizationId,
+
+    updateOrganization,
+    isLoadingUpdateOrganizationMutation,
+    isSuccessUpdateOrganizationMutation,
+    isErrorUpdateOrganizationMutation,
+    ErrorOrganization,
+    localIsResponseUpdateOrganizationMutation,
+  } = useOrganizationHook();
+
+  const [arrayAllPosts, setArrayAllPosts] = useState([]);
+  const [onePost, setOnePost] = useState([]);
+  const [arrayColors, setArrayColors] = useState(new Map());
+  const [isViewChildrenPost, setViewChildrenPost] = useState(false);
+  const [header, setHeader] = useState({
+    postName: "",
+    divisionName: "",
+    fullName: "",
+  });
+
+  const saveUpdateOrganization = async () => {
+    const colorCodes = {};
+    for (const [key, value] of arrayColors) {
+      colorCodes[key] = value;
+    }
+
+    await updateOrganization({
+      _id: reduxSelectedOrganizationId,
+      colorCodes,
+    });
+  };
 
   useEffect(() => {
-    setArray(_.cloneDeep(allPosts));
+    setArrayAllPosts(_.cloneDeep(allPosts));
   }, [allPosts]);
 
+  useEffect(() => {
+    if (
+      currentOrganization?.colorCodes &&
+      typeof currentOrganization.colorCodes === "object"
+    ) {
+      setArrayColors(new Map(Object.entries(currentOrganization.colorCodes)));
+    }
+  }, [currentOrganization]);
+
+  const [openCreatePost, setOpenCreatePost] = useState(false);
+  const createPost = () => {
+    setOpenCreatePost(true);
+  };
+  // console.log(onePost);
   return (
     <div className={classes.dialog}>
-      <Headers name={"схема компании"}></Headers>
+      <Headers name={"схема компании"}>
+        <BottomHeaders update={saveUpdateOrganization} create={createPost} />
+      </Headers>
+
+      <DrawerCreatePost open={openCreatePost} setOpen={setOpenCreatePost}></DrawerCreatePost>
 
       <div className={classes.main}>
-        <h1>Валерий Директор</h1>
+        <div className={classes.header}>
+          <h1 className={classes.boldText}>{header.postName}</h1>
+          <h2 className={classes.boldText}>{header.divisionName}</h2>
+          <h3>{header.fullName}</h3>
+        </div>
+
         <div className={classes.wrapper}>
-          <PostTree posts={array} />
+          {isViewChildrenPost ? (
+            <>
+              {onePost[0]?.children?.map((item) => {
+                return (
+                  <>
+                    <PostSchema
+                      key={item.id}
+                      post={item}
+                      backgroundColor={onePost[0].color}
+                      setOnePost={setOnePost}
+                      setViewChildrenPost={setViewChildrenPost}
+                      setHeader={setHeader}
+                    />
+
+                    <FloatButton
+                      icon={
+                        <img
+                          src={arrowBack}
+                          alt="back"
+                          style={{ width: 20, height: 20 }}
+                        />
+                      }
+                      type="primary"
+                      tooltip="Вернуться назад"
+                      onClick={() => setViewChildrenPost(false)}
+                      style={{
+                        insetInlineEnd: 94,
+                      }}
+                    />
+                  </>
+                );
+              })}
+            </>
+          ) : (
+            <>
+              <PostTree
+                posts={arrayAllPosts}
+                arrayColors={arrayColors}
+                setArrayColors={setArrayColors}
+                setOnePost={setOnePost}
+                setViewChildrenPost={setViewChildrenPost}
+                setHeader={setHeader}
+              />
+            </>
+          )}
         </div>
       </div>
     </div>
---
+++ src/UI/app/CompanySchema/desktop/drawer/Drawer.jsx
@@ -0,0 +1,298 @@
+import React, { useState, useEffect } from "react";
+import ButtonImage from "@Custom/buttonImage/ButtonImage";
+import edit from "@image/edit.svg";
+import { Space, Button, Input, Select, Form, Drawer as DrawerAnt } from "antd";
+import { message } from "antd";
+import { useStatisticsHook } from "../../../../../hooks/useStatisticsHook";
+import { useGetSinglePost, useUpdateSinglePost } from "@hooks";
+
+const { TextArea } = Input;
+
+export default function Drawer({ postId }) {
+  const [open, setOpen] = useState(false);
+  const [shouldFetchData, setShouldFetchData] = useState(false); // Состояние для вызова хука
+  const [formPost] = Form.useForm();
+  const [formStatistics] = Form.useForm();
+  const [isSaving, setIsSaving] = useState(false); // Состояние для блокировки кнопки
+
+  const {
+    // Получить пост по  id
+    currentPost,
+    workers,
+    posts,
+    parentPost,
+    policiesActive,
+    statisticsIncludedPost,
+    isLoadingGetPostId,
+    isErrorGetPostId,
+    isFetchingGetPostId,
+    selectedPolicyIDInPost,
+    selectedPolicyNameInPost,
+    refetchPostIdQuery,
+  } = useGetSinglePost({ postId, enabled: shouldFetchData });
+
+  const {
+    // Обновление поста
+    updatePost,
+    isLoadingUpdatePostMutation,
+    isSuccessUpdatePostMutation,
+    isErrorUpdatePostMutation,
+    ErrorUpdatePostMutation,
+    localIsResponseUpdatePostMutation,
+  } = useUpdateSinglePost();
+
+  const {
+    statistics = [],
+    isLoadingGetStatistics,
+    isFetchingGetStatistics,
+    isErrorGetStatistics,
+
+    updateStatisticsToPostId,
+    isLoadingStatisticsToPostIdMutation,
+    isSuccessUpdateStatisticsToPostIdMutation,
+    isErrorUpdateStatisticsToPostIdMutation,
+    ErrorUpdateStatisticsToPostIdMutation,
+    localIsResponseUpdateStatisticsToPostIdMutation,
+  } = useStatisticsHook();
+
+  const showLoading = () => {
+    setOpen(true);
+    setShouldFetchData(true); // Устанавливаем флаг для вызова хука
+  };
+
+  const handlePostValuesChange = (changedValues, allValues) => {
+    const cleanedValues = Object.fromEntries(
+      Object.entries(allValues).map(([key, value]) => [key, value === undefined ? null : value])
+    );
+    formPost.setFieldsValue(cleanedValues); 
+  };
+
+  const handleSave = async () => {
+    try {
+      setIsSaving(true);
+
+      const [valuesPost, valuesStatistics] = await Promise.all([
+        formPost.validateFields(),
+        formStatistics.validateFields(),
+      ]);
+
+      console.log("valuesPost");
+      console.log(valuesPost);
+
+      const [responsePost, responseStatistics] = await Promise.all([
+        updatePost({
+          _id: currentPost?.id,
+          ...valuesPost,
+        }).unwrap(),
+        ...(valuesStatistics.statisticsIncludedPost.length > 0
+          ? [
+              updateStatisticsToPostId({
+                postId: currentPost.id,
+                ids: valuesStatistics.statisticsIncludedPost,
+              }).unwrap(),
+            ]
+          : []),
+      ]);
+
+      message.success("Данные успешно обновлены!");
+    } catch (error) {
+      if (error.errorFields) {
+        message.error("Пожалуйста, заполните все поля корректно.");
+      } else {
+        message.error("Ошибка при обновлении поста.");
+        console.error("Детали ошибки:", JSON.stringify(error, null, 2));
+      }
+    } finally {
+      setIsSaving(false);
+    }
+  };
+
+  const handleReset = () => {
+    formPost.setFieldsValue({
+      postName: currentPost?.postName ?? null,
+      divisionName: currentPost?.divisionName ?? null,
+      parentId: parentPost?.id ?? null,
+      responsibleUserId: currentPost?.user?.id ?? null,
+      product: currentPost?.product ?? null,
+      purpose: currentPost?.purpose ?? null,
+      policyId: selectedPolicyIDInPost ?? null,
+    });
+    formStatistics.setFieldsValue({
+      statisticsIncludedPost:
+        statisticsIncludedPost.map((stat) => stat.id) ?? null,
+    });
+  };
+
+  useEffect(() => {
+    if (!currentPost || isLoadingGetStatistics || isFetchingGetPostId) return;
+
+    const initialValues = {
+      postName: currentPost?.postName ?? null,
+      divisionName: currentPost?.divisionName ?? null,
+      parentId: parentPost?.id ?? null,
+      responsibleUserId: currentPost?.user?.id ?? null,
+      product: currentPost?.product ?? null,
+      purpose: currentPost?.purpose ?? null,
+      policyId: selectedPolicyIDInPost ?? null,
+    };
+
+    formPost.setFieldsValue(initialValues); // Устанавливаем начальные значения
+  }, [currentPost, isLoadingGetPostId, isFetchingGetPostId]);
+
+  useEffect(() => {
+    if (!statistics || isLoadingGetPostId || isFetchingGetStatistics) return;
+
+    const initialValues = {
+      statisticsIncludedPost:
+        statisticsIncludedPost.map((stat) => stat.id) ?? null,
+    };
+
+    formStatistics.setFieldsValue(initialValues); // Устанавливаем начальные значения
+  }, [statistics, isLoadingGetPostId, isFetchingGetStatistics]);
+
+  return (
+    <div div  style={{with:"100%", marginLeft: "auto"}}>
+      <ButtonImage
+        name={"редактировать"}
+        icon={edit}
+        onClick={showLoading}
+      ></ButtonImage>
+
+      <DrawerAnt
+        closable
+        destroyOnClose2
+        title={<p>Обновление поста</p>}
+        placement="right"
+        open={open}
+        loading={isLoadingGetPostId || isFetchingGetPostId}
+        onClose={() => setOpen(false)}
+      >
+        <Form form={formPost} onValuesChange={handlePostValuesChange} layout="vertical">
+          {/* Название поста */}
+          <Form.Item
+            label="Название поста"
+            name="postName"
+            rules={[
+              { required: true, message: "Пожалуйста, введите название!" },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          {/* Название подразделения */}
+          <Form.Item
+            label="Название подразделения"
+            name="divisionName"
+            rules={[
+              {
+                required: true,
+                message: "Пожалуйста, введите название подразделения!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          {/* Руководитель */}
+          <Form.Item label="Руководитель" name="parentId">
+            <Select
+              allowClear
+              showSearch
+              optionFilterProp="label"
+              filterOption={(input, option) =>
+                option?.label.toLowerCase().includes(input.toLowerCase())
+              }
+              options={posts.map((post) => ({
+                label: post.postName,
+                value: post.id,
+              }))}
+            />
+          </Form.Item>
+
+          {/* Сотрудник */}
+          <Form.Item label="Сотрудник" name="responsibleUserId">
+            <Select
+              allowClear
+              showSearch
+              optionFilterProp="label"
+              filterOption={(input, option) =>
+                option?.label.toLowerCase().includes(input.toLowerCase())
+              }
+              options={workers.map((worker) => ({
+                label: `${worker?.firstName} ${worker?.lastName}`,
+                value: worker.id,
+              }))}
+            />
+          </Form.Item>
+
+          {/* Продукт поста */}
+          <Form.Item
+            label="Продукт поста"
+            name="product"
+            rules={[
+              { required: true, message: "Пожалуйста, введите продукт!" },
+            ]}
+          >
+            <TextArea rows={4} />
+          </Form.Item>
+
+          {/* Предназначение поста */}
+          <Form.Item
+            label="Предназначение поста"
+            name="purpose"
+            rules={[
+              {
+                required: true,
+                message: "Пожалуйста, введите предназначение!",
+              },
+            ]}
+          >
+            <TextArea rows={4} />
+          </Form.Item>
+
+          {/* Политика */}
+          <Form.Item label="Политика" name="policyId">
+            <Select
+              allowClear
+              showSearch
+              optionFilterProp="label"
+              filterOption={(input, option) =>
+                option?.label.toLowerCase().includes(input.toLowerCase())
+              }
+              options={policiesActive.map((policy) => ({
+                label: policy.policyName,
+                value: policy.id,
+              }))}
+            />
+          </Form.Item>
+        </Form>
+
+        <Form form={formStatistics} layout="vertical">
+          {/* Статистика */}
+          <Form.Item label="Статистика" name="statisticsIncludedPost">
+            <Select
+              mode="multiple"
+              showSearch
+              optionFilterProp="label"
+              filterOption={(input, option) =>
+                option?.label.toLowerCase().includes(input.toLowerCase())
+              }
+              options={statistics.map((stat) => ({
+                label: stat.name,
+                value: stat.id,
+                disabled: statisticsIncludedPost.some((s) => s.id === stat.id),
+              }))}
+            />
+          </Form.Item>
+        </Form>
+
+        <Space>
+          <Button type="primary" onClick={handleSave} loading={isSaving}>
+            Сохранить
+          </Button>
+          <Button onClick={handleReset}>Сбросить</Button>
+        </Space>
+      </DrawerAnt>
+    </div>
+  );
+}
