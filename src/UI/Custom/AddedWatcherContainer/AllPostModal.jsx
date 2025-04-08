73949588715776fd6a473cc55428211e5610b07a
arsen4ik228 <belosheikin2003@yandex.ru>
2025-04-08 18:24:14 +0300

    added all post modal for watcher container

--- src/UI/Custom/AddedWatcherContainer/AllPostModal.jsx
+++ src/UI/Custom/AddedWatcherContainer/AllPostModal.jsx
@@ -1,17 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import classes from './AllPostModal.module.css'
 import ModalContainer from '../ModalContainer/ModalContainer'
 import { useAllPosts } from '@hooks'
+import { selectedOrganizationId } from '@helpers/constants'
 
-export default function AllPostModal({setOpenModal}) {
+export default function AllPostModal({ setOpenModal, watchers, buttonClick, selectedPost, setSelectedPost }) {
 
-    const { allPosts } = useAllPosts()
+    const { allPosts } = useAllPosts({ organization: selectedOrganizationId })
+
+    const handlePostClick = (postId) => {
+        setSelectedPost(prevState => {
+            if (prevState.includes(postId)) {
+                return prevState.filter(id => id !== postId); // Удаляем postId
+            } else {
+                return [...prevState, postId]; // Добавляем postId
+            }
+        });
+    };
+
+    console.log(watchers)
+
     return (
         <>
             <ModalContainer
-                buttonText={'Добавить'}
+                buttonText={'Сохранить'}
                 setOpenModal={setOpenModal}
+                clickFunction={buttonClick}
             >
+                <div className={classes.contentContainer}>
+                    {allPosts?.map((item, index) => (
+                        <div key={index} className={classes.postContainer} onClick={() => handlePostClick(item.id)}>
+                            <div className={classes.postName}>
+                                {item.postName}
+                            </div>
+                            <div className={classes.checkboxContainer}>
+                                <input type="checkbox" readOnly checked={selectedPost.includes(item.id)} />
+                            </div>
+                        </div>
+                    ))}
+                </div>
 
             </ModalContainer>
         </>
