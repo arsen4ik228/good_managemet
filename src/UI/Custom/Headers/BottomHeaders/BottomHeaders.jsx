e5c873da6faffe561b025d62aa93778449c0acd9
arsen4ik228 <belosheikin2003@yandex.ru>
2025-06-20 16:08:58 +0300

    added a share button to bottom headers

--- src/UI/Custom/Headers/BottomHeaders/BottomHeaders.jsx
+++ src/UI/Custom/Headers/BottomHeaders/BottomHeaders.jsx
@@ -2,11 +2,18 @@ import React from 'react'
 import classes from "./BottomHeaders.module.css"
 import ButtonAction from './ButtonAction/ButtonAction'
 
-export default function BottomHeaders({children, create, update,  refUpdate, refCreate}) {
+export default function BottomHeaders({ children, shareFunctionList, refShare, create, update, refUpdate, refCreate }) {
   return (
     <div className={classes.editText}>
-        {children}
-        <ButtonAction create={create} update={update} refUpdate={refUpdate} refCreate={refCreate}></ButtonAction>
+      {children}
+      <ButtonAction create={create}
+        update={update}
+        refUpdate={refUpdate}
+        refCreate={refCreate}
+        shareFunctionList={shareFunctionList}
+        refShare={refShare}
+      >
+      </ButtonAction>
     </div>
   )
 }
--- src/UI/Custom/Headers/BottomHeaders/ButtonAction/ButtonAction.jsx
+++ src/UI/Custom/Headers/BottomHeaders/ButtonAction/ButtonAction.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import classes from "./ButtonAction.module.css";
 import iconAdd from "../../../../image/iconAdd.svg";
 import Blacksavetmp from "../../../../image/Blacksavetmp.svg";
 import ButtonImage from "@Custom/buttonImage/ButtonImage";
-export default function ButtonAction({ create, update, refUpdate, refCreate }) {
+import shareIcon from "@Custom/icon/icon _ share.svg";
+import { notEmpty } from "@helpers/helpers"
+
+export default function ButtonAction({ create, update, shareFunctionList, refShare, refUpdate, refCreate }) {
+  const [openShareMenu, setOpenShareMenu] = useState(false);
+  const menuRef = useRef(null);
+
+  // Закрытие меню при клике вне его области
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setOpenShareMenu(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
     <div className={classes.wrapper}>
       {create && (
@@ -12,7 +32,23 @@ export default function ButtonAction({ create, update, refUpdate, refCreate }) {
           name={"Создать"}
           icon={iconAdd}
           onClick={create}
-        ></ButtonImage>
+        />
+      )}
+      {notEmpty(shareFunctionList) && (
+        <div className={classes.shareIconContainer} ref={menuRef}>
+          <ButtonImage
+            refAction={refShare}
+            name={"Поделиться"}
+            icon={shareIcon}
+            onClick={() => setOpenShareMenu(!openShareMenu)}
+          />
+
+          <ul className={`${classes.menuList} ${openShareMenu ? classes.open : ""}`}>
+            {shareFunctionList.map((item, index) => (
+              <li key={index} onClick={item.func} >{item.title}</li>
+            ))}
+          </ul>
+        </div>
       )}
 
       {update && (
@@ -21,7 +57,7 @@ export default function ButtonAction({ create, update, refUpdate, refCreate }) {
           name={"Обновить"}
           icon={Blacksavetmp}
           onClick={update}
-        ></ButtonImage>
+        />
       )}
     </div>
   );
