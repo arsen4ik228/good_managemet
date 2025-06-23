fba92be2a1a908ed7c3125e05510bb3457b0d18b
arsen4ik228 <belosheikin2003@yandex.ru>
2025-06-23 16:23:30 +0300

    "objective" and "Strategies" sections have been combined

--- src/UI/app/StrategyPage/Strategy.jsx
+++ src/UI/app/StrategyPage/Strategy.jsx
@@ -9,9 +9,10 @@ import Headers from "@Custom/Headers/Headers";
 import BottomHeaders from "@Custom/Headers/BottomHeaders/BottomHeaders";
 import SelectBorder from "@Custom/SelectBorder/SelectBorder";
 // import Select from "@Custom/Select/Select";
-import { useStrategyHook } from "@hooks";
-
-import { ConfigProvider, Tour, Select } from "antd";
+import { useStrategyHook, useObjectiveHook } from "@hooks";
+import { notEmpty } from '@helpers/helpers'
+import { Button, ConfigProvider, Tour, Select } from "antd";
+import { EditOutlined, PlusCircleOutlined } from '@ant-design/icons';
 import ruRU from "antd/locale/ru_RU";
 
 const { Option } = Select;
@@ -20,10 +21,16 @@ export default function Strategy() {
   const [number, setNumber] = useState("");
   const [state, setState] = useState("");
   const [editorState, setEditorState] = useState("");
-  console.log(state)
+  const [editMode, setEditMode] = useState(false)
+
+
   const [postId, setPostId] = useState(null);
   const [openModal, setOpenModal] = useState(false);
   const [openModalDraft, setOpenModalDraft] = useState(false);
+
+  const [contentEditors, setContentEditors] = useState([]);
+  const [situationEditors, setSituationEditors] = useState([]);
+  const [rootCauseEditors, setRootCauseEditors] = useState([]);
 
   const selectRef = useRef(null);
   const selectBorderRef = useRef(null);
@@ -82,6 +89,20 @@ export default function Strategy() {
     localIsResponseUpdateStrategyMutation,
   } = useStrategyHook(number);
 
+  const {
+    currentObjective,
+    isLoadingGetObjectiveId,
+    isErrorGetObjectiveId,
+    isFetchingGetObjectiveId,
+
+    updateObjective,
+    isLoadingUpdateObjectiveMutation,
+    isSuccessUpdateObjectiveMutation,
+    isErrorUpdateObjectiveMutation,
+    errorUpdateObjectiveMutation,
+    localIsResponseUpdateObjectiveMutation,
+  } = useObjectiveHook(number);
+
   const stateMapping = {
     Черновик: ["Активный", "Черновик"],
     Активный: ["Активный", "Завершено"],
@@ -106,6 +127,7 @@ export default function Strategy() {
       savePostStarteg();
     }
   };
+
 
   const savePostStarteg = async () => {
     await postStrategy({
@@ -133,7 +155,7 @@ export default function Strategy() {
     }
   };
 
-  const saveUpdateStrateg = async () => {
+  const save1 = async () => {
     const Data = {};
     if (state !== "" && state !== currentStrategy.state) {
       Data.state = state;
@@ -151,6 +173,49 @@ export default function Strategy() {
       })
       .catch((error) => {
         console.error("Ошибка:", JSON.stringify(error, null, 2));
+      });
+  };
+
+  const saveUpdateStrateg = async () => {
+    const Data = {};
+    if (state !== "" && state !== currentStrategy.state) {
+      Data.state = state;
+    }
+    if (editorState !== currentStrategy.content) {
+      Data.content = editorState;
+    }
+
+    try {
+      await updateStrategy({
+        _id: number,
+        ...Data,
+      })
+        .unwrap()
+
+      await updateObjective({
+        _id: currentObjective.id,
+        situation: situationEditors,
+        content: contentEditors,
+        rootCause: rootCauseEditors,
+      })
+        .unwrap()
+    }
+    catch (err) {
+      console.error(err)
+    }
+  }
+
+  const saveUpdateObjective = async () => {
+    await updateObjective({
+      _id: currentObjective.id,
+      situation: situationEditors,
+      content: contentEditors,
+      rootCause: rootCauseEditors,
+    })
+      .unwrap()
+      .then(() => { })
+      .catch((error) => {
+        console.error("Error:", JSON.stringify(error, null, 2));
       });
   };
 
@@ -192,6 +257,39 @@ export default function Strategy() {
     }
   };
 
+  const onEditMode = () => {
+    setEditMode(prev => !prev)
+  }
+
+  const handleEditorChange = (index, newState, type) => {
+    switch (type) {
+      case "content":
+        setContentEditors((prevEditors) => {
+          const updated = [...prevEditors];
+          updated[index] = newState;
+          return updated;
+        });
+        break;
+      case "situation":
+        setSituationEditors((prevEditors) => {
+          const updated = [...prevEditors];
+          updated[index] = newState;
+          return updated;
+        });
+        break;
+      case "rootCause":
+        setRootCauseEditors((prevEditors) => {
+          const updated = [...prevEditors];
+          updated[index] = newState;
+          return updated;
+        });
+        break;
+      default:
+        break;
+    }
+  };
+
+
   useEffect(() => {
     if (postId !== null) {
       setNumber(postId);
@@ -205,7 +303,31 @@ export default function Strategy() {
     if (currentStrategy.state) {
       setState(currentStrategy.state);
     }
+    setEditMode(false)
   }, [currentStrategy.id]);
+
+  useEffect(() => {
+    if (!notEmpty(activeAndDraftStrategies)) return
+
+    const activeStrategy = activeAndDraftStrategies.find(elem => elem.state === 'Активный')
+
+    setNumber(activeStrategy ? activeStrategy.id : activeAndDraftStrategies[0].id)
+
+  }, [activeAndDraftStrategies])
+
+  useEffect(() => {
+    if (Array.isArray(currentObjective.content)) {
+      setContentEditors(currentObjective.content);
+    }
+
+    if (Array.isArray(currentObjective.situation)) {
+      setSituationEditors(currentObjective.situation);
+    }
+
+    if (Array.isArray(currentObjective.rootCause)) {
+      setRootCauseEditors(currentObjective.rootCause);
+    }
+  }, [currentObjective]);
 
   return (
     <div className={classes.dialog}>
@@ -283,12 +405,102 @@ export default function Strategy() {
                   <>
                     {currentStrategy.id ? (
                       <>
+                        {currentStrategy.state !== "Завершено" && (
+                          <div className={classes.editButton}>
+                            <Button
+                              type="primary"
+                              icon={<EditOutlined />}
+                              iconPosition={'end'}
+                              onClick={() => onEditMode()}
+                              style={{
+                                backgroundColor: '#005475',
+                                borderColor: '#005475',
+                              }}
+                            >
+                              Редактировать
+                            </Button>
+                          </div>
+                        )}
+
+                        <div className={classes.title}>
+                          Стратегия
+                        </div>
+
                         <TextArea
                           key={currentStrategy.id}
                           value={editorState}
                           onChange={setEditorState}
-                          readOnly={currentStrategy.state === "Завершено"}
+                          readOnly={!editMode}
                         ></TextArea>
+
+                        <>
+                          <>
+                            <div className={classes.title}>
+                              Краткосрочная цель
+                            </div>
+                            {contentEditors.map((item, index) => (
+                              <TextArea
+                                key={index}
+                                value={item}
+                                onChange={(newState) =>
+                                  handleEditorChange(index, newState, "content")
+                                }
+                                readOnly={!editMode}
+                              ></TextArea>
+                            ))}
+                          </>
+
+                          <>
+                            <div className={classes.title}>
+                              Ситуация
+                            </div>
+                            {situationEditors.map((item, index) => (
+                              <TextArea
+                                key={index}
+                                value={item}
+                                onChange={(newState) =>
+                                  handleEditorChange(index, newState, "situation")
+                                }
+                                readOnly={!editMode}
+                              ></TextArea>
+                            ))}
+                          </>
+
+                          <>
+                            <div className={classes.title}>
+                              Причина
+                            </div>
+                            {rootCauseEditors.map((item, index) => (
+                              <TextArea
+                                key={index}
+                                value={item}
+                                onChange={(newState) =>
+                                  handleEditorChange(index, newState, "rootCause")
+                                }
+                                readOnly={!editMode}
+                              ></TextArea>
+                            ))}
+                          </>
+
+                          <HandlerMutation
+                            Loading={isLoadingUpdateObjectiveMutation}
+                            Error={
+                              isErrorUpdateObjectiveMutation &&
+                              localIsResponseUpdateObjectiveMutation
+                            }
+                            Success={
+                              isSuccessUpdateObjectiveMutation &&
+                              localIsResponseUpdateObjectiveMutation
+                            }
+                            textSuccess={"Краткосрочная цель обновлена"}
+                            textError={
+                              errorUpdateObjectiveMutation?.data?.errors?.[0]?.errors?.[0]
+                                ? errorUpdateObjectiveMutation.data.errors[0].errors[0]
+                                : errorUpdateObjectiveMutation?.data?.message
+                            }
+                          />
+                        </>
+
 
                         <HandlerMutation
                           Loading={isLoadingUpdateStrategyMutation}
