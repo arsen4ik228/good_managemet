7c940ade6c9aa769bd6902162deb170bcf8a4e83
arsen4ik228 <belosheikin2003@yandex.ru>
2025-08-29 12:35:25 +0300

    added edit mode to goal

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -136,3 +136,15 @@ export const transformToString = (arr) => {
   });
   return string
 }
+
+
+export const compareStringArray = (arr1, arr2) => {
+
+  if (arr1.length !== arr2.length) return true
+
+  for(let i=0; i < arr1.length; i++){
+    if(arr1[i] !== arr2[i]){
+      return true
+    }
+  }
+}
