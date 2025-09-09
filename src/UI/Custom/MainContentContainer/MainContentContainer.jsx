2795685a47851c26947091cc905af7359546efc7
arsen4ik228 <belosheikin2003@yandex.ru>
2025-09-09 13:30:33 +0300

    created containers

--- src/UI/Custom/MainContentContainer/MainContentContainer.jsx
+++ src/UI/Custom/MainContentContainer/MainContentContainer.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
 import classes from './MainContentContainer.module.css'
+import { Button } from 'antd'
 
-export default function MainContentContainer({children}) {
+export default function MainContentContainer({ buttons, children }) {
   return (
     <>
-        <div className={classes.wrapper}>
-            <div className={classes.Header}>
-                <div></div>
-                <div></div>
-            </div>
-            <div className={classes.content}>
-                {children}
-            </div>
+      <div className={classes.wrapper}>
+        <div className={classes.Header}>
+          <div></div>
+          <div>
+            {buttons?.map((item, index) => (
+              <Button key={index} onClick={item.click}>{item.text}</Button>
+            ))}
+          </div>
         </div>
+        <div className={classes.content}>
+          {children}
+        </div>
+      </div>
     </>
   )
 }
