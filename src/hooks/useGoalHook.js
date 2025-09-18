5cf26de608424d4e0210b311ca762253b23eb30a
viyaya <johndoe@email.com>
2025-09-18 10:54:46 +0300

    added refetch

--- src/hooks/useGoalHook.js
+++ src/hooks/useGoalHook.js
@@ -1,89 +1,94 @@
 import {
-    useGetGoalQuery,
-    usePostGoalMutation,
-    useUpdateGoalMutation,
-  } from "@services";
-  import useGetReduxOrganization from "./useGetReduxOrganization";
-  import { useMutationHandler } from "./useMutationHandler";
-  
-  export function useGoalHook() {
-    const { reduxSelectedOrganizationId } = useGetReduxOrganization();
-  
-    const {
-      currentGoal = {},
-      isErrorGetGoal,
-      isLoadingGetGoal,
-      isFetchingGetGoal,
-    } = useGetGoalQuery(
-      { organizationId: reduxSelectedOrganizationId },
-      {
-        selectFromResult: ({ data, isLoading, isError, isFetching }) => ({
-          currentGoal: data?.currentGoal || {},
-          isErrorGetGoal: isError,
-          isLoadingGetGoal: isLoading,
-          isFetchingGetGoal: isFetching,
-        }),
-      }
-    );
-  
-    const [
-      updateGoal,
-      {
-        isLoading: isLoadingUpdateGoalMutation,
-        isSuccess: isSuccessUpdateGoalMutation,
-        isError: isErrorUpdateGoalMutation,
-        error: ErrorUpdateGoalMutation,
-        reset: resetUpdateGoalMutation
-      },
-    ] = useUpdateGoalMutation();
-  
-  
-    const localIsResponseUpdateGoalMutation = useMutationHandler(
-      isSuccessUpdateGoalMutation,
-      isErrorUpdateGoalMutation,
-      resetUpdateGoalMutation
-    );
-  
-  
-    const [
-      postGoal,
-      {
-        isLoading: isLoadingPostGoalMutation,
-        isSuccess: isSuccessPostGoalMutation,
-        isError: isErrorPostGoalMutation,
-        error: ErrorPostGoalMutation,
-        reset: resetPostGoalMutation
-      },
-    ] = usePostGoalMutation();
-  
-  
-     const localIsResponsePostGoalMutation = useMutationHandler(
-      isSuccessPostGoalMutation,
-      isErrorPostGoalMutation,
-      resetPostGoalMutation
-      );
-    
-  
-    return {
-      reduxSelectedOrganizationId,
-  
-      currentGoal,
-      isErrorGetGoal,
-      isLoadingGetGoal,
-      isFetchingGetGoal,
-  
-      updateGoal,
-      isLoadingUpdateGoalMutation,
-      isSuccessUpdateGoalMutation,
-      isErrorUpdateGoalMutation,
-      ErrorUpdateGoalMutation,
-      localIsResponseUpdateGoalMutation,
-  
-      postGoal,
-      isLoadingPostGoalMutation,
-      isSuccessPostGoalMutation,
-      isErrorPostGoalMutation,
-      ErrorPostGoalMutation,
-      localIsResponsePostGoalMutation
-    };
-  }
+  useGetGoalQuery,
+  usePostGoalMutation,
+  useUpdateGoalMutation,
+} from "@services";
+import useGetReduxOrganization from "./useGetReduxOrganization";
+import { useMutationHandler } from "./useMutationHandler";
+
+export function useGoalHook() {
+  const { reduxSelectedOrganizationId } = useGetReduxOrganization();
+
+  const {
+    currentGoal = {},
+    isErrorGetGoal,
+    isLoadingGetGoal,
+    isFetchingGetGoal,
+    refetch,
+  } = useGetGoalQuery(
+    { organizationId: reduxSelectedOrganizationId },
+    {
+      selectFromResult: ({
+        data,
+        isLoading,
+        isError,
+        isFetching,
+        refetch,
+      }) => ({
+        currentGoal: data?.currentGoal || {},
+        isErrorGetGoal: isError,
+        isLoadingGetGoal: isLoading,
+        isFetchingGetGoal: isFetching,
+        refetch,
+      }),
+    }
+  );
+
+  const [
+    updateGoal,
+    {
+      isLoading: isLoadingUpdateGoalMutation,
+      isSuccess: isSuccessUpdateGoalMutation,
+      isError: isErrorUpdateGoalMutation,
+      error: ErrorUpdateGoalMutation,
+      reset: resetUpdateGoalMutation,
+    },
+  ] = useUpdateGoalMutation();
+
+  const localIsResponseUpdateGoalMutation = useMutationHandler(
+    isSuccessUpdateGoalMutation,
+    isErrorUpdateGoalMutation,
+    resetUpdateGoalMutation
+  );
+
+  const [
+    postGoal,
+    {
+      isLoading: isLoadingPostGoalMutation,
+      isSuccess: isSuccessPostGoalMutation,
+      isError: isErrorPostGoalMutation,
+      error: ErrorPostGoalMutation,
+      reset: resetPostGoalMutation,
+    },
+  ] = usePostGoalMutation();
+
+  const localIsResponsePostGoalMutation = useMutationHandler(
+    isSuccessPostGoalMutation,
+    isErrorPostGoalMutation,
+    resetPostGoalMutation
+  );
+
+  return {
+    reduxSelectedOrganizationId,
+
+    currentGoal,
+    refetch,
+    isErrorGetGoal,
+    isLoadingGetGoal,
+    isFetchingGetGoal,
+
+    updateGoal,
+    isLoadingUpdateGoalMutation,
+    isSuccessUpdateGoalMutation,
+    isErrorUpdateGoalMutation,
+    ErrorUpdateGoalMutation,
+    localIsResponseUpdateGoalMutation,
+
+    postGoal,
+    isLoadingPostGoalMutation,
+    isSuccessPostGoalMutation,
+    isErrorPostGoalMutation,
+    ErrorPostGoalMutation,
+    localIsResponsePostGoalMutation,
+  };
+}
