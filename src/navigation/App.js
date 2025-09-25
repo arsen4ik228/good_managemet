f7aa525db35356b4c72c877371089c1315804526
viyaya <johndoe@email.com>
2025-09-25 12:15:44 +0300

    added Loader

--- src/navigation/App.js
+++ src/navigation/App.js
@@ -3,60 +3,22 @@ import DesktopApp from "./desktopRouting/DesktopApp";
 import MobileApp from "./mobileRouting/MobileApp";
 import { SocketProvider } from "@helpers/SocketContext.js";
 
-import { Spin } from "antd";
 import { useGlobalLoading } from "@hooks";
+import Loader from "../UI/Custom/Loader/Loader";
+
 
 function App() {
   const loading = useGlobalLoading();
 
   return (
-      <Spin spinning={loading} tip="Загрузка данных..." size="large">
-        <SocketProvider>
-          {isMobile ? <MobileApp /> : <DesktopApp />}
-        </SocketProvider>
-      </Spin>
+    <>
+      {loading && <Loader />}
+
+      <SocketProvider>
+        {isMobile ? <MobileApp /> : <DesktopApp />}
+      </SocketProvider>
+    </>
   );
 }
 
 export default App;
-
-// import { isMobile } from "react-device-detect"; // Импортируем функцию для определения устройства
-// import DesktopApp from "./desktopRouting/DesktopApp";
-// import MobileApp from "./mobileRouting/MobileApp";
-
-// import React from 'react';
-// import { Button, message, Space } from 'antd';
-
-// function App() {
-//   const [messageApi, contextHolder] = message.useMessage();
-//   const success = () => {
-//     messageApi.open({
-//       type: 'success',
-//       content: 'This is a success message',
-//     });
-//   };
-//   const error = () => {
-//     messageApi.open({
-//       type: 'error',
-//       content: 'This is an error message',
-//     });
-//   };
-//   const warning = () => {
-//     messageApi.open({
-//       type: 'warning',
-//       content: 'This is a warning message',
-//     });
-//   };
-//   return (
-//     <>
-//      {contextHolder}
-//       <Space>
-//         <Button onClick={success}>Success</Button>
-//         <Button onClick={error}>Error</Button>
-//         <Button onClick={warning}>Warning</Button>
-//       </Space>
-//   </>
-//   );
-// }
-
-// export default App;
