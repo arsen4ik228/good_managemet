dd567887b38467e14c7a2c06cde940634d5990a8
viyaya <johndoe@email.com>
2025-10-01 13:00:11 +0300

    new create

---
+++ src/UI/layout/SchemaCompany/SchemaCompany.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import schemaCompany from './schemaCompany.svg'
+import MainContentContainer from '../../Custom/MainContentContainer/MainContentContainer'
+
+export function SchemaCompany() {
+
+    return (
+
+        <MainContentContainer>
+            <div style={{
+                width: "630px",
+                minHeight: "100%",
+
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+
+                backgroundColor: "#fff",
+                border: "1px solid #CCCCCC",
+                borderRadius: "5px",
+
+                padding: "0 20px",
+
+                overflow: "hidden",
+            }}>
+                <div style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}>
+                    <img src={schemaCompany} alt="strategy" />
+
+                    <div
+                        style={{
+                            padding: "15px 0",
+                            fontWeight: 600,
+                            fontSize: "20px",
+                            color: "#333333",
+                        }}
+                    >
+                        Схема компании
+                    </div>
+
+                    <div
+                        style={{
+                            fontWeight: 200,
+                            fontSize: "14px",
+                            color: "#333333",
+                        }}
+                    >
+                        Раздел находится в разработке
+                    </div>
+                </div>
+            </div>
+        </MainContentContainer >
+    )
+}
