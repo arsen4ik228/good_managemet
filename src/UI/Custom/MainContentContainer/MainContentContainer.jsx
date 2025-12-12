ce2599d544f83e781e72be392611f26837ad4c53
arsen4ik228 <belosheikin2003@yandex.ru>
2025-12-12 16:28:15 +0300

    added version info

--- src/UI/Custom/MainContentContainer/MainContentContainer.jsx
+++ src/UI/Custom/MainContentContainer/MainContentContainer.jsx
@@ -3,11 +3,17 @@ import classes from './MainContentContainer.module.css'
 import { Button } from 'antd'
 
 export default function MainContentContainer({ component, buttons, children }) {
+
+  const getVersion = () => {
+    const buildDate = process.env.REACT_APP_BUILD_DATE;
+    return buildDate ? `v.${buildDate}` : `v.${new Date().toLocaleDateString('ru-RU')}`;
+}
+
   return (
     <>
       <div className={classes.wrapper}>
         <div className={classes.Header}>
-          <div></div>
+          <div>{getVersion()}</div>
           <div>
             {buttons?.map((item, index) => (
               <Button key={index} onClick={item.click}>{item.text}</Button>
