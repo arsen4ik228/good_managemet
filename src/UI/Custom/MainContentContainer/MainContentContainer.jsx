34fc45b8ae1fa54d79323595e13fdf0a910ea18e
viyaya <johndoe@email.com>
2025-12-16 12:32:51 +0300

    changed пасхалка

--- src/UI/Custom/MainContentContainer/MainContentContainer.jsx
+++ src/UI/Custom/MainContentContainer/MainContentContainer.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
+import { useState } from 'react'
 import classes from './MainContentContainer.module.css'
 import { Button } from 'antd'
 
 export default function MainContentContainer({ component, buttons, children }) {
+  const [count, setCount] = useState(0);
 
   const getVersion = () => {
     const buildDate = process.env.REACT_APP_BUILD_DATE;
     return buildDate ? `v.${buildDate}` : `v.${new Date().toLocaleDateString('ru-RU')}`;
-}
+  }
 
   return (
     <>
       <div className={classes.wrapper}>
-        <div className={classes.Header}>
-          <div>{getVersion()}</div>
+        <div className={classes.Header} onClick={() => setCount(count + 1)}>
+          <div>
+            {
+              count >= 5 && getVersion()
+            }
+          </div>
           <div>
             {buttons?.map((item, index) => (
               <Button key={index} onClick={item.click}>{item.text}</Button>
