e5352456dc4175f8e31bdc54925ca3f722514322
arsen4ik228 <belosheikin2003@yandex.ru>
2026-01-28 12:45:19 +0300

    added project name tio project creation component

--- src/UI/layout/Project/View/ViewProject.jsx
+++ src/UI/layout/Project/View/ViewProject.jsx
@@ -1,6 +1,9 @@
 import { useParams } from "react-router-dom";
 import { useGetSingleProject } from "../../../../hooks/Project/useGetSingleProject";
 import classes from "./ViewProject.module.css";
+import { useEffect } from "react";
+import { useProjectForm } from "../../../../contexts/ProjectFormContext";
+import { notEmpty } from '@helpers/helpers'
 
 const arrayTasks = [
     {
@@ -109,6 +112,19 @@ const TasksContainer = ({ title, tasks }) => {
 export default function ViewProject() {
     const { projectId } = useParams();
     const { currentProject, targets } = useGetSingleProject({ selectedProjectId: projectId });
+
+    const {
+
+        projectName,
+        setProjectName
+    } = useProjectForm();
+
+    useEffect(() => {
+        if (!notEmpty(currentProject)) return
+
+        setProjectName(currentProject?.projectName)
+
+    }, [currentProject])
     return (
         <div className={classes.main}>
             <h3 className={`${classes.strong} ${classes.margin}`}>{localStorage.getItem("name")}</h3>
