6d2b3e87bf2e92946938f84116389d4adb0e9510
valeriy <viaya.lysenko@mail.ru>
2026-02-24 16:37:17 +0300

    added field statusProject

--- src/hooks/Project/useGetSingleProject.js
+++ src/hooks/Project/useGetSingleProject.js
@@ -1,36 +1,39 @@
-import { useGetProjectIdQuery } from "@services/index";
+import {useGetProjectIdQuery} from "@services/index";
 import useGetReduxOrganization from "@hooks/useGetReduxOrganization";
 
 export const useGetSingleProject = ({selectedProjectId}) => {
-  const { reduxSelectedOrganizationId } = useGetReduxOrganization();
+    const {reduxSelectedOrganizationId} = useGetReduxOrganization();
 
-   const {
-     currentProject = {},
-     targets = [],
-     isLoadingGetProjectId,
-     isErrorGetProjectId,
-     isFetchingGetProjectId,
-   } = useGetProjectIdQuery(
-     { projectId: selectedProjectId },
-     {
-       selectFromResult: ({ data, isLoading, isError, isFetching }) => ({
-         currentProject: data?.currentProject || {},
-         targets: data?.targets || [],
-         isLoadingGetProjectId: isLoading,
-         isErrorGetProjectId: isError,
-         isFetchingGetProjectId: isFetching,
-       }),
-       skip: !selectedProjectId, 
-     }
-   );
+    const {
+        currentProject = {},
+        targets = [],
+        statusProject = "",
+        isLoadingGetProjectId,
+        isErrorGetProjectId,
+        isFetchingGetProjectId,
+    } = useGetProjectIdQuery(
+        {projectId: selectedProjectId},
+        {
+            selectFromResult: ({data, isLoading, isError, isFetching}) => ({
+                currentProject: data?.currentProject || {},
+                targets: data?.targets || [],
+                statusProject: data?.statusProject || "",
+                isLoadingGetProjectId: isLoading,
+                isErrorGetProjectId: isError,
+                isFetchingGetProjectId: isFetching,
+            }),
+            skip: !selectedProjectId,
+        }
+    );
 
-  return {
-    reduxSelectedOrganizationId,
+    return {
+        reduxSelectedOrganizationId,
 
-    currentProject,
-    targets,
-    isLoadingGetProjectId,
-    isErrorGetProjectId,
-    isFetchingGetProjectId,
-  };
+        currentProject,
+        targets,
+        statusProject,
+        isLoadingGetProjectId,
+        isErrorGetProjectId,
+        isFetchingGetProjectId,
+    };
 };
