85c025cbf75dc0a289f322002bb39b0f6ff34d3d
valeriy <viaya.lysenko@mail.ru>
2026-03-17 00:46:41 +0300

    added for Button props loading

--- src/UI/Custom/MainContentContainer/MainContentContainer.jsx
+++ src/UI/Custom/MainContentContainer/MainContentContainer.jsx
@@ -22,7 +22,7 @@ export default function MainContentContainer({component, buttons, children, popo
                     <div>
                         <div className={classes.buutonsContainer}>
                             {buttons?.map((item, index) => (
-                                <Button key={index} onClick={item.click}
+                                <Button key={index} onClick={item.click} loading={item?.loading}
                                         style={{color: item?.color}}>{item.text}</Button>
                             ))}
                             {popoverButton}
