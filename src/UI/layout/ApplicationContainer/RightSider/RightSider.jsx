b52e2e5b11f6d7e2ff9795e7de612e1da221a250
valeriy <viaya.lysenko@mail.ru>
2026-03-23 17:24:29 +0300

    change image project

--- src/UI/layout/ApplicationContainer/RightSider/RightSider.jsx
+++ src/UI/layout/ApplicationContainer/RightSider/RightSider.jsx
@@ -61,8 +61,8 @@ export default function RightSider({ config: initialConfig }) {    //
         { id: '11', icon: workingPlan_icon, text: 'Рабочий план', link: 'workingPlan' },
 
         { id: '8', icon: active_strategy, text: 'Стратегия', link: 'strategy', },
-        { id: '9', icon: project, text: 'Проекты', link: 'project', isActive: false },
-        { id: '10', icon: project, text: 'Cтруктура компании', link: 'structure', isActive: true },
+        { id: '9', icon: project, text: 'Проекты', link: 'project', isActive: true },
+        { id: '10', icon: schemaCompany, text: 'Cтруктура компании', link: 'structure', isActive: true },
 
         // { id: '12', icon: project, text: 'Академия', link: 'academy', isActive: false },
         // { id: '13', icon: project, text: 'Корпоративное обучение', link: 'education', isActive: false },
